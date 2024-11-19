147c90bd3e1f603950e7db22c427aa0e04564d3c
Nawras <nawras.othman@yandex.com>
2024-11-19 16:42:08 +0300

    Routing Tutorial

--- daily-quotes/routes/web.php
+++ daily-quotes/routes/web.php
@@ -2,6 +2,84 @@
 
 use Illuminate\Support\Facades\Route;
 
+//*** QUotes
+Route::get('/quotes', function () {
+  require 'data/quotes.php';
+  $output = '<div>';
+  $output .= '<h1>Quotes website</h1>';
+  $output .= '<hr>';
+  $output .= '<ul>';
+  foreach ($quotes as $quote) {
+    $output .= '<li>';
+    $output .= '<a href="' . 'quotes/' . $quote['id'] . '">';
+    $output .= $quote['quote'];
+    $output .= '  - ';
+    $output .= '<span style="color: gray">';
+    $output .= $quote['author'];
+    $output .= '</span>';
+    $output .= '</a>';
+    $output .= '</li>';
+  }
+  $output .= '</ul>';
+
+  $output .= '</div>';
+  return $output;
+});
+
+//** Single Quote
+Route::get('/quotes/{quoteId}', function ($quoteId) {
+  require 'data/quotes.php';
+  $foundQuote = '';
+  foreach ($quotes as $quote) {
+    if ($quote['id'] == $quoteId) {
+      $foundQuote = $quote;
+      break;
+    }
+  }
+  if (empty($foundQuote)) {
+    abort(404);
+  } else {
+    $output = '<div>';
+    $output .= '<h1>';
+    $output .= $foundQuote['quote'];
+    $output .= '</h1>';
+    $output .= '<a href="' . '/quotes' . '">';
+    $output .= 'Go back to quotes';
+    $output .= '</a>';
+    $output .= '</div>';
+    return $output;
+  }
+});
+
+//**  Add Quote
+Route::post('/quotes', function () {
+  return response()->json(
+    [
+      'data' => 'added',
+    ],
+    200,
+  );
+});
+
+Route::delete('/quotes/{quoteId}', function ($quoteId) {
+  return response()->json(
+    [
+      'data' => 'deleted',
+      'id' => $quoteId,
+    ],
+    200,
+  );
+});
+Route::put('/quotes/{quoteId}', function ($quoteId) {
+  return response()->json(
+    [
+      'data' => 'edited',
+      'id' => $quoteId,
+    ],
+    200,
+  );
+});
+
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
